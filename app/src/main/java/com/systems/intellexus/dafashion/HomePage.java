4a2ebb669319c27091f3c537d4ecfa0b62ec59a8
Ajinkya_Kalaskar <ajinkya5130@gmail.com>
2018-01-24 15:12:37 +0530

    function in homepage

--- app/src/main/java/com/systems/intellexus/dafashion/HomePage.java
+++ app/src/main/java/com/systems/intellexus/dafashion/HomePage.java
@@ -2,6 +2,7 @@ package com.systems.intellexus.dafashion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomePage extends AppCompatActivity {
 
@@ -15,5 +16,13 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
+        functionprint1();
+
+    }
+
+    private void functionprint1() {
+        Toast.makeText(this, "works at both side", Toast.LENGTH_SHORT).show();
     }
 }
